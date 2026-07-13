15b692a3b5452bd51ee6bdff92853c55caa017c9
Pavel Kiselyov <kpashka@gmail.com>
2026-07-14 00:14:02 +0100

    Sort databases by name in postgres-init.sql

--- postgres-init.sql
+++ postgres-init.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE identity_auth;
-CREATE DATABASE onboarding_kyc;
 CREATE DATABASE aml_kyt;
-CREATE DATABASE policy_engine;
-CREATE DATABASE wallet_management;
-CREATE DATABASE treasury;
-CREATE DATABASE reconciliation;
-CREATE DATABASE liquidity;
 CREATE DATABASE audit;
+CREATE DATABASE identity_auth;
+CREATE DATABASE liquidity;
+CREATE DATABASE onboarding_kyc;
+CREATE DATABASE policy_engine;
+CREATE DATABASE reconciliation;
+CREATE DATABASE treasury;
+CREATE DATABASE wallet_management;
